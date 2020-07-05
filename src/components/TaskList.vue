3707d863538941c70f3ab287a9515ced01932c5f
Mathieu Ducrot <mathieu.ducrot@pia-production.fr>
2020-07-05 09:39:19 +0200

    Building out each states of the TaskList composite component

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,10 +1,23 @@
 <template>
   <div>
-    <div class="list-items" v-if="loading">loading</div>
-    <div class="list-items" v-if="noTasks && !this.loading">empty</div>
+    <div v-if="loading">
+      <div class="loading-item" v-for="(n, index) in 5" :key="index">
+        <span class="glow-checkbox" />
+        <span class="glow-text">
+          <span>Loading</span> <span>cool</span> <span>state</span>
+        </span>
+      </div>
+    </div>
+    <div class="list-items" v-if="noTasks && !this.loading">
+      <div class="wrapper-message">
+        <span class="icon-check" />
+        <div class="title-message">You have no tasks</div>
+        <div class="subtitle-message">Sit back and relax</div>
+      </div>
+    </div>
     <div class="list-items" v-if="showTasks">
       <task
-        v-for="(task, index) in tasks"
+        v-for="(task, index) in tasksInOrder"
         :key="index"
         :task="task"
         @archiveTask="$emit('archiveTask', $event)"
@@ -15,7 +28,7 @@
 </template>
 
 <script>
-import Task from '@/components/Task'
+import Task from './Task'
 
 export default {
   name: 'task-list',
@@ -38,6 +51,12 @@ export default {
     },
     showTasks() {
       return !this.loading && !this.noTasks
+    },
+    tasksInOrder() {
+      return [
+        ...this.tasks.filter((t) => t.state === 'TASK_PINNED'),
+        ...this.tasks.filter((t) => t.state !== 'TASK_PINNED')
+      ]
     }
   }
 }
