bcb1ea631bddbc0fb1a7e61108c13831f844e706
Mathieu Ducrot <mathieu.ducrot@pia-production.fr>
2020-07-05 11:14:47 +0200

    Supplying store context to Screen stories

--- stories/components/PureInboxScreen.stories.js
+++ stories/components/PureInboxScreen.stories.js
@@ -1,13 +1,37 @@
 //https://www.learnstorybook.com/intro-to-storybook/vue/en/screen/
+import Vue from 'vue'
+import Vuex from 'vuex'
 import PureInboxScreen from '@/components/PureInboxScreen.vue'
+import { action } from '@storybook/addon-actions'
+import { defaultTasksData } from './PureTaskList.stories'
+
+Vue.use(Vuex)
+
+export const store = new Vuex.Store({
+  state: {
+    tasks: defaultTasksData
+  },
+  actions: {
+    pinTask(context, id) {
+      action('pinTask')(id)
+    },
+    archiveTask(context, id) {
+      action('archiveTask')(id)
+    }
+  }
+})
+
 export default {
-  title: 'PureInboxScreen'
+  title: 'PureInboxScreen',
+  // Exclude the store from our stories
+  excludeStories: /.*store$/
 }
 
 // inbox screen default state
 export const Default = () => ({
   components: { PureInboxScreen },
-  template: `<pure-inbox-screen/>`
+  template: `<pure-inbox-screen/>`,
+  store
 })
 
 // inbox screen error state
